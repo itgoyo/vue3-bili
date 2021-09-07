d3026c5f6488617ac81a053d98d2dd0903afcc7f
itgoyo <itgoyo@foxmail.com>
2021-09-07 22:16:27 +0800

    一分钟请求一次

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,9 @@ import request from '../utils/request'
 
 export function getData() {
   return request.get(
-    "/api/x/relation/stat?vmid=12767066"
+    // "/api/x/relation/stat?vmid=12767066"
     // "/api/room/v1/Room/getRoomInfoOld?mid=12767066"
+    "/api/x/space/acc/info?mid=12767066"
+
   );
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,9 +19,7 @@
 </template>
 
 <script setup>
-  import {
-    reactive
-  } from "vue";
+  import {onMounted,reactive} from "vue";
   import {
     getData
   } from "../api/api";
@@ -30,16 +28,69 @@
     msg: String,
   });
 
+  var options = {
+  dir: "auto", // 文字方向
+  body: "👿👿👿👿推流直播已经结束👿👿👿👿", // 通知主体
+  requireInteraction: true, // 不自动关闭通知
+  // 通知图标
+  icon: "http://localhost:8080/hacker.png"
+};
+
+
+function notifyMe(title, options) {
+  // 先检查浏览器是否支持
+  if (!window.Notification) {
+    console.log('浏览器不支持通知');
+  } else {
+    // 检查用户曾经是否同意接受通知
+    if (Notification.permission === 'granted') {
+      var notification = new Notification(title, options); // 显示通知
+    } else if (Notification.permission === 'default') {
+      // 用户还未选择，可以询问用户是否同意发送通知
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('用户同意授权');
+          var notification = new Notification(title, options); // 显示通知
+        } else if (permission === 'default') {
+          console.warn('用户关闭授权 未刷新页面之前 可以再次请求授权');
+        } else {
+          // denied
+          console.log('用户拒绝授权 不能显示通知');
+        }
+      });
+    } else {
+      // denied 用户拒绝
+      console.log('用户曾经拒绝显示通知');
+    }
+  }
+}
+
+
+function callMe() {
+  console.log("通知");
+}
   async function fetchData() {
     const res = await getData();
-    console.log(res);
+    console.log(res.data.live_room.liveStatus);
+    if (res.data.live_room.liveStatus === 1) {
+      // notifyMe()
+      notifyMe('这是通知的标题', options);
+    }
+
+    // var json = JSON.parse(res)
   }
   var resData;
+
 
   fetchData();
   const state = reactive({
     count: 0
   });
+
+ this.timer = setInterval(function(){
+   fetchData()
+  },60*1000)//一分钟请求一次
+
 </script>
 
 <style scoped>
